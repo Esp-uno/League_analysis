b12af9c60de2be28129f1d7a5387af496a5b776b
Esp-uno <abdalla.a4457@gmail.com>
2026-02-02 23:51:55 +0000

    Project change

--- Lg_analysis_c.py
+++ Lg_analysis_c.py
@@ -1,61 +1,111 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait         
-from selenium.webdriver.support import expected_conditions as EC   
-from bs4 import BeautifulSoup
+
 import pandas as pd
 import logging
 import matplotlib.pyplot as plt
+import yfinance as yf
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.metrics import precision_score
+import numpy as np
 
-def get_data():
-        try:
-                #Using selenium to open up fbref 
-                print("Opening chrome browser... no logging whatsoever")
-                driver = webdriver.Chrome()
-                driver.get("https://fbref.com/en/comps/8/2024-2025/2024-2025-Champions-League-Stats")
+sp500 = yf.Ticker("^GSPC")
 
-                #wait until the elements have loaded 
-                print("Waiting for webpage to load... weeee are here")
-                wait = WebDriverWait(driver, 10)
-                wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "table_container")))
+history = sp500.history(period = "max")
 
-                #Find the web elements
-                league_phase_table = driver.find_element(By.ID, "all_results2024-202582")
+index = history.index
+
+del history['Stock Splits']
+
+del history['Dividends']
+
+
+
+#graph closing price against the index
+
+def close_v_user():
+    x = history.index
+    y = history['Close']
+    plt.plot(x,y)
+    plt.xlabel('Date')
+    plt.ylabel('Closing price')
+    plt.show()
+
+
+df_sp500 = history
+
+#Create column tomorrow and set it as the closing price from the day before
+
+df_sp500["Tomorrow"] = df_sp500['Close'].shift(-1)
+
+# if tomorrows price is greater than the closing price return 1 else return 0
+
+df_sp500['Target'] = (df_sp500['Tomorrow']>df_sp500['Close']).astype(int)
+
+#Get rid of anything before 2008
+
+df_sp500 = df_sp500.loc['2008-01-01':].copy()
+
+def learning_and_precision():
+    
+    model = RandomForestClassifier(n_estimators=300,min_samples_split=100,random_state=1)
+    train = df_sp500.iloc[:-100]
+    test = df_sp500.iloc[-100:]
+
+    predictors = ['High','Low','Open','Close','Volume']
+    model.fit(train[predictors],train['Target'])
+
+    #create prediction
+    pred = model.predict(test[predictors]) 
+    pred = pd.Series(pred,index= test.index)
+
+    #generate a precision score
+    precision = precision_score(test['Target'],pred)
+
+    combined = pd.concat(test['Target'],pred, axis=1)
+
+    return model, precision, combined
+
+
+def backtesting(data, model, predictors, start= 2500, step= 2):
+    
+    model = RandomForestClassifier(n_estimators=300, min_samples_split=100, random_state=1)
+    predictors = ['High', 'Low', 'Open', 'Close', 'Volume']
+
+    total_predictions = []
+
+    
+    if not isinstance(data, pd.DataFrame):
+        data = pd.DataFrame(data)
+
+
+    for i in range(start,data.shape[0],step):
+        train = data.iloc[0:i].copy()
+        test = data.iloc[i:(i+step)].copy()
+        current_predictions = predictors(train, test, current_predictions, model)
+        total_predictions.append(current_predictions)
+
+    predictions = backtesting(df_sp500, model, predictors)
+    precision = precision_score(predictions['Target'],predictions['Predictions'])
+    print(precision)
+
+    return pd.concat(total_predictions), predictions
+    
+backtesting()
         
-                #get inner html content    
-                league_phase_html = league_phase_table.get_attribute("innerHTML")
-                
-                #convert raw html to beautiful soup objects
-                print("Converting raw HTML to beautifulsoup...")
-                league_phase_soup = BeautifulSoup(league_phase_html, "html.parser")
-                table = league_phase_soup.find_all('table')
-                pandas_table = pd.read_html(str(table))[0]
-                pandas_table.drop("Notes", axis=1, inplace=True)
-                print(pandas_table)
-                return pandas_table
-                
-        finally:
-                
-                driver.quit()
 
 
 
-def data_visuals(pandas_table):
-       
-        #Goals for vs Expected goals
-        plt.scatter(pandas_table['Squad'],pandas_table['GF'], label = 'Goals for')
-        plt.scatter(pandas_table['Squad'],pandas_table['xG'], label = 'Expected goals')
-        plt.xticks(rotation = 45, ha='right')
-        max_goals = int(pandas_table[['GF', 'xG']].max().max()) + 1
-        plt.yticks(range(0, max_goals + 1, 1))
-        plt.title("Goals compared to Expected Goals")
-        plt.xlabel('Team')
-        plt.ylabel('Goals')
-        plt.legend()
-        plt.show()
+   
+    
+
+
+
+
+
+
+
+
         
 
-pandas_table = get_data()
-data_visuals(pandas_table)
+           
 
 
